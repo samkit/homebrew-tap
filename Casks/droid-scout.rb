bf405a5ecd69894abae6d6600f34a0e8d0b8ce85
samkit <1732809+samkit@users.noreply.github.com>
2026-06-02 21:53:41 +0530

    Update Droid Scout to v0.1.10

--- Casks/droid-scout.rb
+++ Casks/droid-scout.rb
@@ -1,6 +1,6 @@
 cask "droid-scout" do
-  version "0.1.11"
-  sha256 "c07a8c2fec27a1e38e75ce78912dc997f0d7c9b14d057baf994021e3ee45fef2"
+  version "0.1.10"
+  sha256 "5f0ca4f27dcd95844f0b9fad0a3d97498139915927e5f0c4d6b18a07e50693be"
 
   url "https://github.com/samkit/droid-scout/releases/download/v#{version}/DroidScout-#{version}.zip",
       verified: "github.com/samkit/droid-scout/"
